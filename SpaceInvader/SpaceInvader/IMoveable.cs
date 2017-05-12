55fae606481a3c5c5d3ff88f8a71ecfa8ee9f92d
DorionQc <gouletsamuel@hotmail.com>
2017-05-12 09:59:01 -0400

    IMoveable
    Interface pour les entités pouvant bouger

---
+++ SpaceInvader/SpaceInvader/IMoveable.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SpaceInvader
+{
+    public interface IMoveable
+    {
+        Vector2 Velocity { get; set; }
+        Vector2 Position { get; set; }
+        void Update();
+    }
+}
